9fd28f01bf52a049bce20020b9413d90255fea55
Kunszt Norbert <kunszt.norbert@gmail.com>
2024-07-01 21:27:34 +0200

    update invoice by id

--- routes/invoicesRoutes.js
+++ routes/invoicesRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express'),
   router = express.Router();
 
-router.get('/list', function(req, res) {
+router.get('/list', function (req, res) {
   let sql = `SELECT * FROM invoices`;
-  db.query(sql, function(err, data, fields) {
+    db.query(sql, function (err, data, fields) {
     if (err) throw err;
     res.json({
       status: 200,
@@ -12,6 +12,36 @@ router.get('/list', function(req, res) {
     })
   })
 });
+
+
+router.patch('/update/:id?', function (req, res) {
+  let sql = "UPDATE invoices"
+  
+  const invoiceId=req.params.id||null
+  const keys=["name","date","paid","checked","price"]
+  if(Object.keys(req.body).length>0){
+    sql += " SET "
+  }
+for(let i=0;i<keys.length;i++){
+  if(req.body.hasOwnProperty(keys[i])) sql += `${keys[i]}="${req.body[keys[i]]}" ,`
+}
+sql=sql.substring(0,sql.length-1)
+sql += ` WHERE id=${invoiceId} `
+
+db.query(sql, function (err, data, fields) {
+  if (err) throw err;
+  res.json({
+    status: 200,
+    data,
+    message: "Invoice updated"
+  })
+})
+
+  
+  
+})
+
+
 
 
 module.exports = router;
