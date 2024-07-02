3e1f398e445cbe31f65a9b3f5491e72207744ac2
Kunszt Norbert <kunszt.norbert@gmail.com>
2024-07-02 12:06:06 +0200

    create address

--- routes/addressRouutes.js
+++ routes/addressRouutes.js
@@ -20,6 +20,44 @@ router.get('/list', function (req, res) {
 
 
 
+router.post('/create', function (req, res) {
+  let sql = "INSERT INTO addresses values"
+
+  // kulcsok
+  const keys = [
+    "name",
+    "postal_code",
+    "address",
+    "phone",
+    "city",
+    "same_data",
+    "measuring_point"
+  ]
+  sql += " (null,"
+
+  // ellenőrzi a kulcsokat
+  for (let i = 0; i < keys.length; i++) {
+    if (req.body.hasOwnProperty(keys[i])) sql += `"${req.body[keys[i]]}",`
+  }
+  sql += " null,null)"
+
+  db.query(sql, function (err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 200,
+      data,
+      message: "Address created"
+    })
+  })
+
+
+
+})
+
+
+
+
+
 router.patch('/update/:id?', function (req, res) {
   let sql = "UPDATE addresses"
 
